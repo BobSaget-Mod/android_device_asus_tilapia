464497231a95d86e844ea7e4ebf69a75b601576d
Paul Beeler <sparksco@gmail.com>
2013-01-17 14:06:57 -0700

    Add missing packages

--- gsm.mk
+++ gsm.mk
@@ -1,8 +1,14 @@
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Mms \
-    Stk
+    rild \
+    Stk \
+    VoiceDialer
 
 # System dump APN config
 PRODUCT_COPY_FILES += \
     device/asus/tilapia/system/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    keyguard.no_require_sim=true \
+    ro.com.android.dataroaming=false
