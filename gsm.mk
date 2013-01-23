98fce9a9d5b5fc9e2f06818202ac1c3aae97b9d8
Paul Beeler <sparksco@gmail.com>
2013-01-23 01:52:54 -0700

    Fix typos in headers

--- gsm.mk
+++ gsm.mk
@@ -1,4 +1,4 @@
-# SIM Toolkit
+# Extra Required SaberMod packages
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver \
     Mms \
@@ -10,6 +10,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/asus/tilapia/system/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+# Extra gsm properties
 PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true \
     ro.com.android.dataroaming=false
